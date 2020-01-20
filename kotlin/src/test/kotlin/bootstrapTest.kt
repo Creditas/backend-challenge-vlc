9b0bade52cf526418921222c73936abca0602de7
victormuramoto <victor.muramoto@gmail.com>
2020-01-20 13:13:32 +0100

    change specs

--- kotlin/src/test/kotlin/bootstrapTest.kt
+++ kotlin/src/test/kotlin/bootstrapTest.kt
@@ -7,9 +7,9 @@ class BootstrapTest {
     @Test
     fun `when adding product to order`() {
         val order = Order(Customer(), Address())
-        val music = Product("Stairway to Heaven", ProductType.DIGITAL, 5.00)
-        val book = Product("The Hitchhiker's Guide to the Galaxy", ProductType.BOOK, 120.00)
-         val shirt = Product("Flowered t-shirt", ProductType.PHYSICAL, 35.00)
+        val music = Product("Product 1", ProductType.DIGITAL, 10.00)
+        val book = Product("Product 2", ProductType.BOOK, 11.00)
+         val shirt = Product("Product 3", ProductType.PHYSICAL, 12.00)
 
         order.addProduct(shirt, 1)
         order.addProduct(book, 1)
@@ -21,13 +21,13 @@ class BootstrapTest {
     @Test
     fun `when total amount is calculated`() {
         val order = Order(Customer(), Address())
-        val music1 = Product("Highway to hell", ProductType.DIGITAL, 10.00)
-        val music2 = Product("Stairway to Heaven", ProductType.DIGITAL, 15.00)
+        val music1 = Product("Product 1", ProductType.DIGITAL, 10.00)
+        val music2 = Product("Product 2", ProductType.BOOK, 15.00)
 
 
-        order.addProduct(music1, 1)
-        order.addProduct(music2, 1)
+        order.addProduct(music1, 2)
+        order.addProduct(music2, 2)
 
-        assertThat(order.totalAmount).isEqualTo(50)
+        assertThat(order.totalAmount).isEqualTo(30)
     }
 }
